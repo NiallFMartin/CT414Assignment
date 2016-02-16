2bb239c1c61bea1e690554db617e9a5245395647
Niall Martin <github@niallmartin.com>
2016-02-16 16:51:44 +0000

    Made changes to client.java

--- CT414Assignment/src/Client/Client.java
+++ CT414Assignment/src/Client/Client.java
@@ -11,42 +11,45 @@ import Server.ExamServer;
 
 public class Client {
 
-	public static void main (String [] args){
-
-		String name = "ExamServer"; int num = 0;
-		
+	private static String name = "ExamServer"; 
+	private static int num = 0;
+	
+	
+	public static void main (String [] args){		
 		if (System.getSecurityManager() == null){
 			System.setSecurityManager(new RMISecurityManager());
 		}
 		
 		while (num == 0){
-			try {
-				
-				ExamServer client = (ExamServer) Naming.lookup(name);
-				
-				Scanner scanner = new Scanner(System.in);
-				System.out.println("Enter your User ID(123): ");
-				int userID = scanner.nextInt();
-				System.out.println("Enter your password(abc): ");
-				String password = scanner.next();
-				
-				System.out.println("Attempting to login...");
-				System.out.println("Login: " + client.login(userID, password));
-				if (client.login(userID, password) == 1){
-					System.out.println("\nYou are now logged in :)");
+			signin();
+		}
+	}
+	
+	private static void signin(){
+		try {
+			ExamServer client = (ExamServer) Naming.lookup(name);
+			
+			Scanner scanner = new Scanner(System.in);
+			System.out.println("Enter your User ID(123): ");
+			int userID = scanner.nextInt();
+			System.out.println("Enter your password(abc): ");
+			String password = scanner.next();
+			
+			System.out.println("Attempting to login...");
+			if (client.login(userID, password) == 1){
+				System.out.println("\nYou are now logged in :)");
+				System.exit(0);
+			} else {
+				System.out.println("\nWrong login :(");
+				System.out.println("Enter 0 to try again: \nOR \n1 to exit: ");
+				num = scanner.nextInt();
+				if (num != 0){
 					System.exit(0);
-				} else {
-					System.out.println("\nWrong login :(");
-					System.out.println("Enter 0 to try again: \nOR \n1 to exit: ");
-					num = scanner.nextInt();
-					if (num != 0){
-						System.exit(0);
-					}
 				}
-				
-			} catch (Exception e) {
-				e.printStackTrace();
 			}
+			
+		} catch (Exception e) {
+			e.printStackTrace();
 		}
 	}
 	
