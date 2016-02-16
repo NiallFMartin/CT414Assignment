1010aa1c04ff7c6f7b3ff8eef03ea58993c15e0f
Shane O' Rourke <shaneorourke974@gmail.com>
2016-02-16 14:42:43 +0000

    Started QuestionImplementation.java.

--- CT414Assignment/src/Server/QuestionImplementation.java
+++ CT414Assignment/src/Server/QuestionImplementation.java
@@ -2,22 +2,26 @@ package Server;
 
 public class QuestionImplementation implements Question{
 
-	@Override
+	private int qNum; //Question Number
+	private String qDetail; //Question Detail
+	private String [] qAnsOptions; //Question Answer Options
+	
+	public QuestionImplementation(int qNum, String qDetail, String [] qAnsOptions){
+		qNum = this.qNum;
+		qDetail = this.qDetail;
+		qAnsOptions = this.qAnsOptions;
+	}
+	
 	public int getQuestionNumber() {
-		// TODO Auto-generated method stub
-		return 0;
+		return this.qNum;
 	}
 
-	@Override
 	public String getQuestionDetail() {
-		// TODO Auto-generated method stub
-		return null;
+		return this.qDetail;
 	}
 
-	@Override
 	public String[] getAnswerOptions() {
-		// TODO Auto-generated method stub
-		return null;
+		return this.qAnsOptions;
 	}
 
 }
